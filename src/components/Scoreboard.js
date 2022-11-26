65b9586b556e7961b974c5ad701b0d0ff088968e
AvresCode <servas230@gmail.com>
2022-11-26 18:34:15 +0100

    Added sort by name and change order function

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -6,15 +6,38 @@ const compareScore = (playerA, playerB) => {
   return playerB.score - playerA.score;
 };
 
+const compareName = (playerA, playerB) => {
+  return playerA.name.localeCompare(playerB.name);
+};
+
 export const Scoreboard = () => {
   const [players, setPlayers] = useState(PlayerData);
 
+  const [sortBy, setSortBy] = useState("score");
+
   //sort players
-  const sortedPlayers = [...players].sort(compareScore);
+  //   const sortedPlayers = [...players].sort(compareScore);
+
+  const sortedPlayers = [...players].sort(
+    sortBy === "score" ? compareScore : compareName
+  );
+
+  //change order
+  const changeOrder = (event) => {
+    setSortBy(event.target.value);
+  };
 
   //
   return (
     <div className="scoreboard">
+      <div>
+        {" "}
+        Change order :{" "}
+        <select onChange={changeOrder} value={sortBy}>
+          <option value="score"> Sort by score </option>
+          <option value="name"> Sort by name </option>
+        </select>
+      </div>
       <p> Scoreboard's players:</p>
       <ul>
         {sortedPlayers.map((player) => (
