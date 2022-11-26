f81b935f70ce22c46b69d1c90e7b3ffed593369d
AvresCode <servas230@gmail.com>
2022-11-26 14:35:19 +0100

    Sorted players based on score

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -2,13 +2,22 @@ import { useState } from "react";
 import { Player } from "./Player";
 import { PlayerData } from "./PlayerData";
 
+const compareScore = (playerA, playerB) => {
+  return playerB.score - playerA.score;
+};
+
 export const Scoreboard = () => {
   const [players, setPlayers] = useState(PlayerData);
+
+  //sort players
+  const sortedPlayers = [...players].sort(compareScore);
+
+  //
   return (
     <div className="scoreboard">
       <p> Scoreboard's players:</p>
       <ul>
-        {players.map((player) => (
+        {sortedPlayers.map((player) => (
           //   <Player name={player.name} key={player.id} />
           <Player {...player} key={player.id} />
         ))}
